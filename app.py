33234516412495f01dcd4dee42d1bfc2bedeb373
Admin <admin@MacBook-Pro-Admin-2.local>
2021-08-28 23:35:21 +0300

    ADD - keymap logic UPDATE - keymap binding

--- app.py
+++ app.py
@@ -1,32 +1,48 @@
 import os
 import json
-from typing import Union
 from functools import partial
+
 
 from pythonosc.udp_client import SimpleUDPClient
 import keyboard
 from atem_board.board import Board
 
 
-SETTINGS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-        "settings.json")
+SETTINGS_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "settings.json"
+)
+KEYMAP_PATH = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "keymap.json")
+
 
 def get_settings() -> dict:
     with open(SETTINGS_PATH, "r") as f:
         return json.load(f)
 
+
+def get_keymap() -> dict:
+    with open(KEYMAP_PATH, "r") as f:
+        return json.load(f)
+
+
 def get_client(ip: str, port: int):
     return SimpleUDPClient(ip, port)
 
+
 def main() -> None:
     settings = get_settings()
+    key_map = get_keymap()
     board = Board(settings["server_ip"], int(settings["server_port"]))
-    send_5 = partial(board._send_message, message="/atem/program/5")
-    send_6 = partial(board._send_message, message="/atem/program/6")
-    keyboard.hook_key("5", send_5)
-    keyboard.hook_key("6", send_6)
-
-
+    print("[INFO] Binding ...")
+    for key, msg in key_map.items():
+        callback = partial(board._send_message, message=msg)
+        keyboard.hook_key(key, callback, suppress=True)
+    # keyboard.add_hotkey('p', lambda: print('space was pressed!'))
+    print("[INFO] Running ...")
     keyboard.wait("0")
+    keyboard.unhook_all()
+    print("[INFO] Stopped")
+
+
 if __name__ == "__main__":
     main()
--- atem_board/board.py
+++ atem_board/board.py
@@ -1,8 +1,7 @@
-import os
-import json
-from typing import Union, Optional, Any
+from typing import Any
 
 from pythonosc.udp_client import SimpleUDPClient
+
 
 class Board(object):
     def __init__(self, ip: str, port: int) -> None:
@@ -13,4 +12,3 @@ class Board(object):
     def _send_message(self, event, message: str, param: Any = None) -> None:
         if event.event_type == "up":
             self._client.send_message(message, param)
-
